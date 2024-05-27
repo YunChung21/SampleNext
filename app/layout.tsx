6ac87b5c3d1248dbfca9a2ebdbce5774e29fabbf
40224617 <zx96748520@gmail.com>
2024-05-27 04:48:05 -0700

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,21 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-NZ3L9CKR');</script>
-      </head>
-      <body className={`${inter.className} antialiased`}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZ3L9CKR"
-            height="0" width="0" style="display:none;visibility:hidden">
-          </iframe>
-        </noscript>
-        {children}
-        </body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
